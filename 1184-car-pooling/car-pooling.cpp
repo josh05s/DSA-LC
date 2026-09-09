675a7d6c386d5762597cff07fb2b6a81cf121587
Joshua Smith <Josh.smith7023@gmail.com>
2026-09-09 11:48:55 +1000

    Time: 7 ms (11.33%) | Memory: 14 MB (95.07%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> diff(1001, 0);
-
-        for (int i = 0; i < trips.size(); i++) {
-            int numPassengers = trips[i][0];
-            int from = trips[i][1];
-            int to = trips[i][2];
-
-            diff[from] += numPassengers;
-            diff[to] -= numPassengers;
-        }
-
-        int currentPassengers = 0;
-        for (int i = 0; i <= 1000; i++) {
-            currentPassengers += diff[i];
-            if (currentPassengers > capacity) {
+        for (int location = 0; location <= 1000; location++) {
+            int total = 0;
+            
+            for (int i = 0; i < trips.size(); i++) {
+                int numPassengers = trips[i][0];
+                int from = trips[i][1];
+                int to = trips[i][2];
+                
+                if (from <= location && location < to) {
+                    total += numPassengers;
+                }
+            }
+            
+            if (total > capacity) {
                 return false;
             }
         }
-
+        
         return true;
     }
 };

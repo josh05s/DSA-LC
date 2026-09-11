220ec66a595512940720e4404df49fe897b5da6a
Joshua Smith <Josh.smith7023@gmail.com>
2026-09-11 20:10:30 +1000

    Time: 3 ms (27.77%) | Memory: 95.1 MB (60.89%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) { // 2nd approach
+        if (nums1.size() > nums2.size())
+        return findMedianSortedArrays(nums2, nums1);
+        
+        int m = nums1.size();
+        int n = nums2.size();
 
-        vector<int> numsTotal(n + m, 0);
+        int lo = 0, hi = m;
 
-        for (int i = 0; i < n; i++) {
-            numsTotal[i] = nums1[i];
+        while (lo <= hi) {
+            int i = lo + (hi - lo)/2;
+            int j = (n + m + 1)/2 - i;
+
+            int left1 = (i == 0) ? INT_MIN : nums1[i - 1];
+            int right1 = (i == m) ? INT_MAX : nums1[i];
+            int left2 = (j == 0) ? INT_MIN : nums2[j - 1];
+            int right2 = (j == n) ? INT_MAX : nums2[j];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((m + n) % 2 == 1) {
+                    return max(left1, left2);
+            } else {
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                }
+            } else if (left1 > right2) {
+                hi = i - 1; // took too much from nums1, shrink
+            } else {
+                lo = i + 1; // took too little from nums1, grow
+            }
         }
 
-        for (int i = 0; i < m; i++) {
-            numsTotal[i + n] = nums2[i];
+        return -1.0;
+
         }
-
-        sort(numsTotal.begin(), numsTotal.end());
-
-        int size = numsTotal.size();
-        if (size % 2 == 0) {
-            return (numsTotal[size/2] + numsTotal[(size/2) - 1])/2.0;
-        }
-
-        return numsTotal[size/2];
-    }
 };

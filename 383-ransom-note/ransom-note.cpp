e520988b3ecf8abc9bd115a5abe74d86c709b07a
Joshua Smith <Josh.smith7023@gmail.com>
2026-09-24 15:09:09 +1000

    Time: 0 ms (100.00%) | Memory: 11.9 MB (49.00%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int counts[26] = {0};
+
+        for (char c : magazine) {
+            counts[c - 'a']++;
+        }
+
+        for (char c : ransomNote) {
+            counts[c - 'a']--;
+            if (counts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
